f7efb9abb6d80591ab1ef9f1076f3e96e49a7bf8
21120326 <doantanqn000@gmail.com>
2023-11-21 00:46:42 +0700

    Added script file back in test

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -32,7 +32,7 @@ int main()
     }
 
     PrintString("\n Before sorting: \n");
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         PrintChar(' ');
         PrintInt(A[i]);
     }
@@ -47,7 +47,7 @@ int main()
             }
 
     PrintString("\n After sorting: \n");
-    for (int i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) {
         PrintChar(' ');
         PrintInt(A[i]);
     }
