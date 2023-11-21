0f69c09bbaaeeff08a669681eb4ec2c0d7f850dc
21120326 <doantanqn000@gmail.com>
2023-11-22 00:09:10 +0700

    Code up create.c file

--- nachos-3.4/code/test/create.c
+++ nachos-3.4/code/test/create.c
@@ -2,4 +2,20 @@
 #define MAX_LENGTH 32
 
 int main() {
+    char fileName[MAX_LENGTH];
+    PrintString("\n\t\t\t --------TAO FILE-------- \n\n");
+
+    PrintString(" - Nhap ten file: ");
+    ReadString(fileName, MAX_LENGTH);
+
+    if (strlen(fileName) == 0 || strlen(fileName) == 1) {
+        strcpy(fileName, "text.txt");
+    }
+
+    if (Create(fileName) == 0) {
+        PrintString("\n Tao file thanh cong.");
+    }
+    else {
+        PrintString("\n Loi trong qua trinh tao file.");
+    }
 }
