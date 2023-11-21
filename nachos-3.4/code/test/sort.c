a9567fd7f3dc051d8807d4d659bf18956238896a
21120326 <doantanqn000@gmail.com>
2023-11-21 19:22:38 +0700

    Modification in sort.c 5

--- nachos-3.4/code/test/sort.c
+++ nachos-3.4/code/test/sort.c
@@ -40,7 +40,7 @@ main()
 
     /* Sort */
     for (i = 0; i < n - 1; i++)
-        for (j = 0; j < (n - i - i); j++)
+        for (j = 0; j < (n - i - 1); j++)
             if (A[j] > A[j + 1]) {
                 tmp = A[j];
                 A[j] = A[j + 1];
