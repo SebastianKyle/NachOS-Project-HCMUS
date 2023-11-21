19f2c363f16f45f15e14a44efc696e006f166dc8
21120326 <doantanqn000@gmail.com>
2023-11-21 16:28:48 +0700

    Modification in help.c

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -18,6 +18,7 @@ main() {
     PrintString("\n\n Cac chuong trinh: ");
     PrintString("\n +) sort: cho phep nguoi dung nhap vao 1 mang n so nguyen sau do sap xep mang");
     PrintString("\n +) ascii: in ra bang ma ASCII");
+    PrintString("\n\n");
 
     Halt();
 }
