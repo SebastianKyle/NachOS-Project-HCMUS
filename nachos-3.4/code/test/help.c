c7429727bbb9f0100968095d248dbc6d958b1adb
21120326 <doantanqn000@gmail.com>
2023-11-22 13:14:03 +0700

    Modification in help.c

--- nachos-3.4/code/test/help.c
+++ nachos-3.4/code/test/help.c
@@ -18,6 +18,7 @@ main() {
     PrintString("\n\n Cac chuong trinh: ");
     PrintString("\n +) sort: cho phep nguoi dung nhap vao 1 mang n so nguyen sau do sap xep mang bang Bubble sort");
     PrintString("\n +) ascii: in ra bang ma ASCII");
+    PrintString("\n +) create: cho phep nguoi dung tao file voi ten tuy chon");
     PrintString("\n\n");
 
     Halt();
